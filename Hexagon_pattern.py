2f682d3ad48934a6497b96fc32d2143b4329e30e
AnshMNSoni <anshsoni702@gmail.com>
2024-05-24 12:36:04 +0530

    Hexagon pattern

---
+++ Hexagon_pattern.py
@@ -0,0 +1,31 @@
+# Hexagon Pattern:
+
+import turtle
+
+tt = turtle.Turtle()
+
+turtle.bgcolor("black")
+tt.speed("fastest")
+tt.penup()
+tt.goto(0, 200)
+tt.back(100)
+tt.pendown()
+
+clr = ["blue", "pink", "red", "yellow", "orange", "green"]
+
+side = 6
+for i in range(6, 250, 6):
+    for j in range(6):
+        tt.right(1)
+        tt.pencolor(clr[j])
+        tt.forward(i)
+        tt.right(60)
+    
+    # Move the pen to another position
+    tt.penup()
+    tt.left(120)
+    tt.forward(5)
+    tt.right(120)
+    tt.pendown()
+
+turtle.done()
